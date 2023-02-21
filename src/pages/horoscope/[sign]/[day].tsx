eb764e5dbd9bd9ada2ea67d93dea0278e1ee459a
catlynbowles <bowlescatlyn@gmail.com>
2023-02-21 13:40:57 -1000

    feat: add loading component

--- src/pages/horoscope/[sign]/[day].tsx
+++ src/pages/horoscope/[sign]/[day].tsx
@@ -1,8 +1,10 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import {fetchHoroscopeData} from '../../api/fetch'
+import Loading from "@/components/Loading";
 
 export default function Results() {
+  const [loading, setLoading] = useState(true)
   const [results, setResults] = useState({
     current_date: '', 
     description: '',
@@ -12,26 +14,33 @@ export default function Results() {
     lucky_time: ''
   })
 
+  
+
   const router = useRouter();
   const {sign, day} = router.query
   console.log(sign, day)
 
   useEffect(() => {
     fetchHoroscopeData(sign, day)
-      .then(data => setResults(data))
+      .then(data => {
+        setResults(data)
+        setLoading(false)
+      })
       .then(it => console.log(it))
   }, [sign])
 
   return (
     <div>
-      {sign} horoscope for {results.current_date}
+      {loading ? <Loading /> :
       <div>
+        {sign} horoscope for {results.current_date}
         <p>{results.description}</p>
         <p>The Mood: {results.mood}</p>
         <p>Lucky Number: {results.lucky_number}</p>
         <p>Best Friend Today: {results.compatibility}</p>
         <p>Lucky Time: {results.lucky_time}</p>
       </div>
+}
     </div>
   );
 }
