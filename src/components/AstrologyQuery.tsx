2944e1779497da518fe489e900a5a5ef327608f8
catlynbowles <bowlescatlyn@gmail.com>
2023-02-24 09:39:29 -1000

    feat: add firebase db to app

--- src/components/AstrologyQuery.tsx
+++ src/components/AstrologyQuery.tsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
 import { signs, days } from "@/pages/api/data";
 import Dropdown from "./Dropdown";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "@/firebase";
+import { userAgent } from "next/server";
 
-export default function AstrologyQuery() {
+export default function AstrologyQuery({ user }: any) {
   const [sign, setSign] = useState("");
   const [day, setDay] = useState("");
+  console.log(user)
+
+  const addUserSign = async (e: any) => {
+    e.preventDefault();
+    console.log(user);
+    if (user) {
+      const docRef = await addDoc(collection(db, "sign"), {
+        user: user.uid,
+        sign: sign,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } else {
+      console.error("Error adding document: ", e);
+    }
+  };
 
   return (
     <form
-      action={`/horoscope/${sign}/${day}`}
+      // action={`/horoscope/${sign}/${day}`}
       className="flex flex-col items-center"
     >
       <label className="text-3xl">Which stars will you align today?</label>
@@ -19,8 +37,9 @@ export default function AstrologyQuery() {
         placeholder="what's your sign?"
       />
       <br />
-      <Dropdown items={days} setChange={setDay} placeholder="select a day" />
+      {/* <Dropdown items={days} setChange={setDay} placeholder="select a day" /> */}
       <button
+        onClick={(e) => addUserSign(e)}
         type="submit"
         className="bg-black text-white m-5 p-1 hover:bg-white hover:text-black"
       >
--- src/pages/horoscope/[sign]/[day].tsx
+++ src/pages/horoscope/[sign]/[day].tsx
@@ -6,70 +6,72 @@ import Loading from "@/components/Loading";
 import Prompt from "@/components/Prompt";
 import { HoroscopeType, HoroscopeDefaultValues } from "../../../types/types";
 
-type Sign = {
-  sign: string;
-};
 
 export default function Results() {
-  const randomSign = useRef("");
-  const [loading, setLoading] = useState(true);
-  const [horoscope, setHoroscope] = useState<HoroscopeType>(
-    HoroscopeDefaultValues
-  );
-  const [decoyHoroscope, setDecoyHoroscope] = useState<HoroscopeType>(
-    HoroscopeDefaultValues
-  );
-
-  const router = useRouter();
-  const { sign, day } = router.query;
-
-  useEffect(() => {
-    if (sign && day) {
-      let filteredHoroscopes = signs.filter((symbol) => symbol !== sign);
-      randomSign.current =
-        filteredHoroscopes[
-          Math.floor(Math.random() * filteredHoroscopes.length)
-        ];
-
-      Promise.all([
-        fetchHoroscopeData(sign, day),
-        fetchHoroscopeData(randomSign.current, day),
-      ]).then((data) => {
-        data[0].name = sign;
-        setHoroscope(data[0]);
-        data[1].name = randomSign.current;
-        setDecoyHoroscope(data[1]);
-        setLoading(false);
-      });
-    }
-  }, [sign, day]);
-
-  return (
-    <div>
-      {loading ? (
-        <Loading />
-      ) : (
-        <div>
-          {sign} horoscope for {horoscope.current_date}
-          <Prompt
-            title="How do you feel?"
-            description={horoscope.description}
-          />
-          <Prompt title="What's the mood?" description={horoscope.mood} />
-          <Prompt title="What's the color?" description={horoscope.color} />
-          <Prompt title="Lucky number?" description={horoscope.lucky_number} />
-          <Prompt
-            title="Who was your bestie?"
-            description={horoscope.compatibility}
-          />
-          <Prompt
-            title="What was the best time of day?"
-            description={horoscope.lucky_time}
-          />
-        </div>
-      )}
-    </div>
-  );
+ return (
+  <div>results</div>
+ )
 }
 
+// export default function Results() {
+//   const randomSign = useRef("");
+//   const [loading, setLoading] = useState(true);
+//   const [horoscope, setHoroscope] = useState<HoroscopeType>(
+//     HoroscopeDefaultValues
+//   );
+//   const [decoyHoroscope, setDecoyHoroscope] = useState<HoroscopeType>(
+//     HoroscopeDefaultValues
+//   );
+
+//   const router = useRouter();
+//   const { sign, day } = router.query;
+
+//   useEffect(() => {
+//     if (sign && day) {
+//       let filteredHoroscopes = signs.filter((symbol) => symbol !== sign);
+//       randomSign.current =
+//         filteredHoroscopes[
+//           Math.floor(Math.random() * filteredHoroscopes.length)
+//         ];
+
+//       Promise.all([
+//         fetchHoroscopeData(sign, day),
+//         fetchHoroscopeData(randomSign.current, day),
+//       ]).then((data) => {
+//         data[0].name = sign;
+//         setHoroscope(data[0]);
+//         data[1].name = randomSign.current;
+//         setDecoyHoroscope(data[1]);
+//         setLoading(false);
+//       });
+//     }
+//   }, [sign, day]);
+
+//   return (
+//     <div>
+//       {loading ? (
+//         <Loading />
+//       ) : (
+//         <div>
+//           {sign} horoscope for {horoscope.current_date}
+//           <Prompt
+//             title="How do you feel?"
+//             description={horoscope.description}
+//           />
+//           <Prompt title="What's the mood?" description={horoscope.mood} />
+//           <Prompt title="What's the color?" description={horoscope.color} />
+//           <Prompt title="Lucky number?" description={horoscope.lucky_number} />
+//           <Prompt
+//             title="Who was your bestie?"
+//             description={horoscope.compatibility}
+//           />
+//           <Prompt
+//             title="What was the best time of day?"
+//             description={horoscope.lucky_time}
+//           />
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
 // an app that says identify if it's yours or someone else's !
